743568c746b889c5fba0730f30ef6e003aad5d7d
sarathchandra0111 <113340047+sarathchandra0111@users.noreply.github.com>
2024-06-01 11:40:25 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("PYTHON PROGRAMMING")
+st.subheader("python program for finding largest among 2 numbers")
+st.code("""a=10
+b=20
+if a<b:
+    print("b is greater")
+else:
+    print("a is greater")
+        """)
+
+
+st.subheader("python program to add two numbers using functions")
+st.code("""def add(a,b):
+    return(a+b)
+print(add(3,4))
+        """)
